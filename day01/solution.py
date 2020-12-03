b1c88745d0e82ed4bef6d10b244ee962cdec5ed4
Michał Kret <krtexxx@gmail.com>
2020-12-03 21:07:34 +0100

    Day 1.1: refactor befor 2nd part.

--- day01/solution.py
+++ day01/solution.py
@@ -1,16 +1,21 @@
 def read_input_into_set(name):
     return set(map( int, set(line.strip() for line in open(name))))
 
-def find_sum(name):
-    report = read_input_into_set(name)
+def find_complement(addend, sum, report):
+    complement = sum - addend
+    if complement in report:
+        return complement
+    return None
 
+def find_sum_of_two(report):
     for entry in report:
-        rest = 2020 - entry
-        if rest in report:
-            print(entry, "x", rest, "=", entry*rest)
+        complement = find_complement(entry, 2020, report)
+        if(complement):
+            print(entry, "x", complement, "=", entry*complement)
             return
     print("Pair not found")
     return
 
 if __name__ == "__main__":
-    find_sum("input.txt")
+    report = read_input_into_set("input.txt")
+    find_sum_of_two(report)
