fbcd1bb944e7e352b1c8adc2599be1b42eabe9a5
Michał Kret <krtexxx@gmail.com>
2020-12-03 23:39:59 +0100

    Day 2.2: extended part 1 solved part 2 as well.

--- day02/solution.py
+++ day02/solution.py
@@ -13,18 +13,30 @@ class Entry:
             return True
         return False
 
+    def is_new_password_correct(self):
+        result = False
+        if self.password[self.lower-1] == self.letter:
+            result = not result
+        if self.password[self.upper-1] == self.letter:
+            result = not result
+        return result
+
 def parse_line(line):
     found = re.search('(\d+)-(\d+) ([a-z]): ([a-z]+)', line)
     result = Entry(int(found.group(1)), int(found.group(2)), found.group(3), found.group(4))
-    return result.is_password_correct()
+    return result.is_password_correct(), result.is_new_password_correct()
 
 if __name__ == "__main__":
-    correct_pwds = 0
+    old_pwds = 0
+    new_pwds = 0
     with open("input.txt") as input:
         lines = input.readlines()
         for line in lines:
-            if parse_line(line):
-                correct_pwds += 1
+            old_pwd_policy, new_pwd_policy = parse_line(line)
+            if old_pwd_policy:
+                old_pwds += 1
+            if new_pwd_policy:
+                new_pwds += 1
 
-    print("Correct passwords:", correct_pwds)
+    print("Correct passwords:", old_pwds, new_pwds)
 
