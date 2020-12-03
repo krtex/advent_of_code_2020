5f99b9c78b5f4e86495bd37547634ab402537c20
Michał Kret <krtexxx@gmail.com>
2020-12-03 21:47:48 +0100

    Day 1.2: not that pretty but works. O(n^2)

--- day01/solution.py
+++ day01/solution.py
@@ -12,10 +12,25 @@ def find_sum_of_two(report):
         complement = find_complement(entry, 2020, report)
         if(complement):
             print(entry, "x", complement, "=", entry*complement)
-            return
+            return complement
     print("Pair not found")
-    return
+    return None
+
+def find_sum_of_three(report):
+    ugly_thing_to_do = list(report)
+
+    for first in range(len(ugly_thing_to_do)):
+        for second in range(first + 1, len(ugly_thing_to_do)):
+            sum = ugly_thing_to_do[first] + ugly_thing_to_do[second]
+            if(sum < 2020):
+                third = find_complement(sum, 2020, report)
+                if(third):
+                    print(ugly_thing_to_do[first], "x", ugly_thing_to_do[second], "x", third, "=", ugly_thing_to_do[first] * ugly_thing_to_do[second] * third)
+                    return
+
 
 if __name__ == "__main__":
     report = read_input_into_set("input.txt")
+
     find_sum_of_two(report)
+    find_sum_of_three(report)
