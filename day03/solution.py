dee49a9ab533065e18f7e4316154e2f8ab9562b3
Michał Kret <krtexxx@gmail.com>
2020-12-05 21:54:58 +0100

    Day 3.2: Counted trees on other slopes too!

--- day03/solution.py
+++ day03/solution.py
@@ -1,20 +1,23 @@
+from math import prod
+
 def load_lines(input):
     with open(input) as file:
         lines = file.readlines()
     lines = [l.strip() for l in lines]
     return lines
 
-def count_trees(slope):
+def count_trees(slope, right=3, down=1):
     tree_count = 0
     for idx, line in enumerate(slope):
-        print(line)
-        if line[(3 * idx) % len(line)] == '#':
-            print("TREE!")
-            tree_count += 1
+        if not (idx % down):
+            if line[(right * idx) % len(line)] == '#':
+                tree_count += 1
     return tree_count
 
 if __name__ == "__main__":
     slope = load_lines("input.txt")
-    result = count_trees(slope)
 
-    print("There were", result, "trees on my way!")
+    paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    grand_result = [count_trees(slope, *i) for i in paths]
+    print("Trees on given slopes:", grand_result, "and their product:", prod(grand_result))
+
