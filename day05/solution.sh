9236d31662c3c96287347eab73d1054b82feac11
Michał Kret <krtexxx@gmail.com>
2020-12-06 00:50:13 +0100

    Day 5.1: Lol, console one-line (almost) did the job xD

---
+++ day05/solution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bin_value=$(sed -e 's/F/0/g' -e 's/B/1/g' -e 's/R/1/g' -e 's/L/0/g' input.txt | sort | tail -n1)
+# This line only exists to strip the following value from windows  character as bs got a problem with it:/
+bin_value=$(echo "$bin_value" | tr -d $'\r')
+echo "obase=10; ibase=2; $bin_value" | bc
