b225bf4f16b2c3f496290c9fffa500922bf4cc53
Michał Kret <krtexxx@gmail.com>
2020-12-06 20:32:05 +0100

    Day 6.2: Part 2 in progress with intersecion of sets.

--- day06/solution.py
+++ day06/solution.py
@@ -3,11 +3,23 @@ import re
 def count_answers_in_group(group):
     return len(set(group.replace('\n', '')))
 
+def check_common_part(group):
+    answers = re.split(' |\r\n|\n', group)
+    answers = [set(a) for a in answers]
+    return len(set.intersection(*answers))
+
 def read_input(input):
     with open(input) as file:
-        groups = re.split('\r\n\r\n|\n\n', file.read())
-        return [count_answers_in_group(group) for group in groups]
+        return re.split('\r\n\r\n|\n\n', file.read())
 
+def count_questions_anyone_answered(groups):
+    return [count_answers_in_group(group) for group in groups]
+
+def count_questions_everyone_answered(groups):
+    return [check_common_part(group) for group in groups]
 
 if __name__ == "__main__":
-    print("Sum of all answers", sum(read_input("input.txt")))
+    groups = read_input("input.txt")
+    tests = read_input("test.txt")
+    print("Sum of all answers", sum(count_questions_anyone_answered(groups)))
+    print("Sum of common answers", sum(count_questions_everyone_answered(tests)))
